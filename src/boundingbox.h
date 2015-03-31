ee3f2e5fb3645cf6acb706ade9d873b8d38d514e
Aldo Culquicondor <aldo.culquicondor@ucsp.edu.pe>
2015-03-30 21:12:20 -0500

    Siguiendo estilo de codigo Google

--- src/boundingbox.h
+++ src/boundingbox.h
@@ -8,43 +8,43 @@
 
 template <typename Traits>
 class BoundingBox {
-    typedef typename Traits::CoordType CoordType;
-    static const size_t dimensions_ = Traits::dimensions_;
-    typedef typename Traits::Interval Interval;
-public:
-    BoundingBox(std::initializer_list<Interval> intervals);
+  typedef typename Traits::CoordType CoordType;
+  static const size_t dimensions_ = Traits::dimensions_;
+  typedef typename Traits::Interval Interval;
+ public:
+  BoundingBox(std::initializer_list<Interval> intervals);
 
-    Interval GetInterval(size_t dimension) const {
-        return intervals_[dimension];
-    }
-    CoordType GetIntervalStart(size_t dimension) const {
-        return intervals_[dimension].first;
-    }
-    CoordType GetIntervalEnd(size_t dimension) const {
-        return intervals_[dimension].second;
-    }
+  Interval GetInterval(size_t dimension) const {
+    return intervals_[dimension];
+  }
+  CoordType GetIntervalStart(size_t dimension) const {
+    return intervals_[dimension].first;
+  }
+  CoordType GetIntervalEnd(size_t dimension) const {
+    return intervals_[dimension].second;
+  }
 
-    void SetInterval(size_t dimension, Interval interval) {
-       intervals_[dimension] = interval;
-    }
-    void SetIntervalStart(size_t dimension, CoordType coord) {
-        intervals_[dimension].first = coord;
-    }
-    void SetIntervalEnd(size_t dimension, CoordType coord) {
-        intervals_[dimension].second = coord;
-    }
+  void SetInterval(size_t dimension, Interval interval) {
+    intervals_[dimension] = interval;
+  }
+  void SetIntervalStart(size_t dimension, CoordType coord) {
+    intervals_[dimension].first = coord;
+  }
+  void SetIntervalEnd(size_t dimension, CoordType coord) {
+    intervals_[dimension].second = coord;
+  }
 
-private:
-    Interval intervals_[dimensions_];
+ private:
+  Interval intervals_[dimensions_];
 };
 
 template <typename T>
 BoundingBox<T>::BoundingBox(std::initializer_list<typename BoundingBox::Interval> intervals) {
-    size_t i = 0;
-    if (intervals.size() != dimensions_)
-        throw RStarException("Dimensions mismatch");
-    for (const Interval &interval : intervals)
-        intervals_[i] = interval;
+  size_t i = 0;
+  if (intervals.size() != dimensions_)
+    throw RStarException("Dimensions mismatch");
+  for (const Interval &interval : intervals)
+    intervals_[i] = interval;
 }
 
 #endif //BSTAR_TREE_BOUNDINGBOX_H
--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main() {
-    RStarTree<> rtree;
-    RStarTree<>::BoundingBox bb{{1, 2}, {2, 3}, {4, 5}};
-    return 0;
+  RStarTree<> rtree;
+  RStarTree<>::BoundingBox bb{{1, 2}, {2, 3}, {4, 5}};
+  return 0;
 }
