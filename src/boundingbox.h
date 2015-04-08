60a99a3341fa7dc20344c8f8f0e0cf83b9d45f15
Dante Nuñez <dante.cnp@gmail.com>
2015-04-07 21:42:38 -0500

    entandarizando identacion

--- src/boundingbox.h
+++ src/boundingbox.h
@@ -47,26 +47,26 @@ class BoundingBox {
 
 template <typename T>
 BoundingBox<T>::BoundingBox(std::initializer_list<typename BoundingBox::Interval> intervals) {
-    size_t i = 0;
-    if (intervals.size() != dimensions_)
-        throw RStarException("Dimensions mismatch");
-    for (const Interval &interval : intervals){
-        intervals_[i] = interval;
-        ++i;
-    }
-    area_ = -1;
+  size_t i = 0;
+  if (intervals.size() != dimensions_)
+      throw RStarException("Dimensions mismatch");
+  for (const Interval &interval : intervals){
+      intervals_[i] = interval;
+      ++i;
+  }
+  area_ = -1;
 }
 
 template <typename T>
 typename BoundingBox<T>::CoordType BoundingBox<T>::HyperArea()
 {
-    BoundingBox<T>::CoordType res = 1;
-    if (area_ != -1)
-        return this->area_ = res;
-    for (const typename BoundingBox<T>::Interval &interval : BoundingBox<T>::intervals_){
-        res *= std::get<1>(interval) - std::get<0>(interval);
-    }
-    return this->area_ = res;
+  BoundingBox<T>::CoordType res = 1;
+  if (area_ != -1)
+      return this->area_ = res;
+  for (const typename BoundingBox<T>::Interval &interval : BoundingBox<T>::intervals_){
+      res *= std::get<1>(interval) - std::get<0>(interval);
+  }
+  return this->area_ = res;
 
 }
 
