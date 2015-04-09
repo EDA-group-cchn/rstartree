0dc75ad890d48c49acd9e6b747a1a40e5f837252
Dante Nuñez <dante.cnp@gmail.com>
2015-04-08 21:25:15 -0500

    agregando la funcion enlargement

--- src/boundingbox.h
+++ src/boundingbox.h
@@ -38,8 +38,9 @@ class BoundingBox {
   }
 
   CoordType HyperArea();
-
+  CoordType Enlargement(const BoundingBox<Traits> &bb);
   CoordType Overlap(const BoundingBox<Traits> &bb);
+
 
   bool Intersects(const BoundingBox<Traits> &bounding_box) const;
   bool operator%(const BoundingBox<Traits> &bounding_box) const {
@@ -82,6 +83,17 @@ typename BoundingBox<T>::CoordType BoundingBox<T>::HyperArea() {
     res *= interval.second - interval.first;
   }
   return this->area_ = res;
+}
+
+template <typename T>
+typename BoundingBox<T>::CoordType BoundingBox<T>::Enlargement(
+    const BoundingBox<T> &bb) {
+  BoundingBox<T>::CoordType res = 1;
+  for (size_t i = 0; i < T::dimensions_; ++i) {
+    res *= std::max(this->intervals_[i].second, bb.intervals_[i].second) -
+           std::min(this->intervals_[i].first, bb.intervals_[i].first);
+  }
+  return res;
 }
 
 template <typename T>
